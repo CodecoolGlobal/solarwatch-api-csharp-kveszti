2c2799b39f397a25d12201a4003708a5a0768722
Kiss-Virág Eszter <144778442+kveszti@users.noreply.github.com>
2024-07-17 09:47:54 +0200

    authcontroller steup done

--- SolarWatch/Services/Authentication/IAuthService.cs
+++ SolarWatch/Services/Authentication/IAuthService.cs
@@ -3,4 +3,6 @@ namespace SolarWatch.Services.Authentication;
 public interface IAuthService
 {
     Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
+    
+    Task<AuthResult> LoginAsync(string email, string password);
 }
