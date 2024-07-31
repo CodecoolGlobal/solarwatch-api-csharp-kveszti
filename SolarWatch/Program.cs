562ac58dca3a80f5eed76bd97d21ede3920d98df
Kiss-Virág Eszter <144778442+kveszti@users.noreply.github.com>
2024-07-31 11:46:07 +0200

    adding csproj files

---
+++ SolarWatch/Program.cs
@@ -0,0 +1,169 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SolarWatch;
+using SolarWatch.Context;
+using SolarWatch.Services;
+using SolarWatch.Services.Authentication;
+using SolarWatch.Services.Repositories;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
+AddServices();
+ConfigureSwagger();
+AddDbContexts();
+AddAuthentication();
+AddIdentity();
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    var frontendURL = configuration.GetValue<string>("frontend_url");
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+    });
+});
+
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var authenticationSeeder = scope.ServiceProvider.GetRequiredService<AuthenticationSeeder>();
+authenticationSeeder.AddRoles();
+authenticationSeeder.AddAdmin();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
+
+
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+    builder.Services.AddSingleton<IGeocoding, Geocoding>();
+    builder.Services.AddSingleton<ISolarApi, SunriseSunsetApi>();
+    builder.Services.AddScoped<ICityRepository, CityRepository>();
+    builder.Services.AddScoped<ISolarDataRepository, SolarDataRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<AuthenticationSeeder>(); 
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "SolarWatch", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
+}
+
+void AddDbContexts()
+{
+    var connectionString = builder.Configuration.GetConnectionString("SolarApi");
+    
+    if (builder.Environment.IsEnvironment("Testing"))
+    {
+        // Configure for testing environment
+        builder.Services.AddDbContext<SolarApiContext>(options => 
+            options.UseInMemoryDatabase("TestingDb"));
+        builder.Services.AddDbContext<UsersContext>(options => 
+            options.UseInMemoryDatabase("TestingDb"));
+    }
+    else
+    {
+        builder.Services.AddDbContext<SolarApiContext>(options => 
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDbContext<UsersContext>(options => 
+            options.UseSqlServer("Server=localhost,1433;Database=SolarApi;User Id=sa;Password=YourStrongPassword123!;TrustServerCertificate=true;HostNameInCertificate=localhost;"));
+    }
+}
+
+void AddAuthentication()
+{
+    var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+    var issuerSigningKey = builder.Configuration["JwtSettings:IssuerSigningKey"];
+    
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["ValidIssuer"],
+                ValidAudience = jwtSettings["ValidAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerSigningKey)
+                ),
+            };
+        });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<UsersContext>();
+}
+public partial class Program { }
